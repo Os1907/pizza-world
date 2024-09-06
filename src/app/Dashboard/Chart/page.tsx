96d0b089b1847470870080d0aabd71d1691b7c36
os1907 <osamatrezey@gmail.com>
2024-09-06 15:37:31 +0300

    fix

--- src/app/Dashboard/Chart/page.tsx
+++ src/app/Dashboard/Chart/page.tsx
@@ -7,12 +7,16 @@ import Loading from '@/app/_Components/Loading/Loading'
 import { calcAverage, calculateTotalEarnings, earningsLastMonth } from '@/helper/statics/statics'
 import UseHydration from '@/Hooks/useHydration'
 import { useEarning, useItemCount } from '@/Hooks/useSelectors'
+import dynamic from 'next/dynamic'
 import React from 'react'
+
+const DynamicCharts = dynamic(() => import('@/app/_Components/DashoardComponents/Chart/chart'), { ssr: false });
+const DynamicTinyBarChart = dynamic(() => import('@/app/_Components/DashoardComponents/Chart/TinyBarChart'), { ssr: false });
+const DynamicTopOrder = dynamic(() => import('@/app/_Components/DashoardComponents/Chart/topOrder'), { ssr: false });
 
 export default function Page() {
     const amounts = useEarning()
     const itemCounts = useItemCount()
-    console.log(itemCounts)
     const totalEarningsData = calculateTotalEarnings(amounts)
     const amountslast = earningsLastMonth
     const revenue = calcAverage(amounts, 0.75)
@@ -32,38 +36,33 @@ export default function Page() {
     }
     return (
         <>
-            <section className=' min-h-screen bg-dark px-4'>
-
-                {
-                    hydration ? <div className='pt-10 pb-14  '>
-                        <h4 className='text-body px-6 font-extrabold text-center lg:text-start text-4xl lg:text-6xl mb-4'>
-                            Sales Statics
-                        </h4>
-                        <div className="grid grid-cols-6 m-4 gap-4 ">
-                            <EaDetails data={totalEarningsData} />
-                            <div className='col-span-6'>
-                                
-                                <Charts current={amounts} last={amountslast} text={"Earnings Comparison in Current Month  and Last Month"} size='2xl'  x='Current Month' y='Last Month'/>
-                            </div>
-
-                            {codeRepetition(<TinyBarChart current={revenue} last={expenses} text='Comparison between Revenue and Expenses for Current month' />)}
-                            {codeRepetition(<Charts current={revenueLast} last={expensesLast} text=' Comparison between Revenue and Expenses for Last month' size='base' x='Revenue' y='Expenses'/>)}
-
-                            <div className='col-span-6'>
-                                <TopOrder count={itemCounts} text=" Top Orders (% in Sales)" size='2xl'/>
-                            </div>
-
-
+        <section className='min-h-screen bg-dark px-4'>
+            {hydration ? (
+                <div className='pt-10 pb-14'>
+                    <h4 className='text-body px-6 font-extrabold text-center lg:text-start text-4xl lg:text-6xl mb-4'>
+                        Sales Statics
+                    </h4>
+                    <div className="grid grid-cols-6 m-4 gap-4 ">
+                        <EaDetails data={totalEarningsData} />
+                        <div className='col-span-6'>
+                            <DynamicCharts current={amounts} last={amountslast} text={"Earnings Comparison in Current Month and Last Month"} size='2xl' x='Current Month' y='Last Month' />
                         </div>
 
-                    </div> :  <div className='min-h-screen flex items-center'><Loading shape='box' /></div> 
-                }
+                        {codeRepetition(<DynamicTinyBarChart current={revenue} last={expenses} text='Comparison between Revenue and Expenses for Current month' />)}
+                        {codeRepetition(<DynamicCharts current={revenueLast} last={expensesLast} text='Comparison between Revenue and Expenses for Last month' size='base' x='Revenue' y='Expenses' />)}
 
-
-            </section>
-
-
-        </>
+                        <div className='col-span-6'>
+                            <DynamicTopOrder count={itemCounts} text="Top Orders (% in Sales)" size='2xl' />
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className='min-h-screen flex items-center'>
+                    <Loading shape='box' />
+                </div>
+            )}
+        </section>
+    </>
     )
 }
 
